848a25dbc65bfe3fd456204e7d862f92bca0cd77
Bob <bobdt84@gmail.com>
2022-04-24 10:10:03 -0500

    Added attr_reader to TicTacToe and Player class.  Session inherits from TicTacToe and registers players to start a new session

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,11 @@
 class TicTacToe
+  attr_reader :player1, :player2
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
   end
-  def fill_board(array,width = 3)
+  
+  def print_board(array,width = 3)
     #would it be better/easier to handle a nested array?
     board = []
     h_spacer = "\t---" + "|---"*(width-1) + "\n"
@@ -17,43 +19,49 @@ class TicTacToe
         board.push("| #{value} ")
       end
     end
-    board.join()
+    puts board.join()
   end
 end
 
 class Player
+  attr_reader :name, :token, :record
   def initialize(name,token)
     @name = name
     @token = token
     @record = { won: 0, lost: 0, draw: 0}
   end
+
 end
 
-#class Session < TicTacToe
-def start_game
-  player1 = register_player("Player1")
-  player2 = register_player("Player2")
-  game = TicTacToe.new(player1,player2)
-end
+class Session < TicTacToe
+  def initialize(board_size=[3,3])
+    super register_player("Player1"), register_player("Player2")
+    @board_size = board_size
+  end 
 
-def register_player(player)
-  print "Name of #{player}: "
-  name = gets.strip
-  print "#{player}'s Marking Token: "
-  token = gets.strip
-  Player.new(name,token)
+  private
+  def register_player(player)
+    print "Name of #{player}: "
+    name = gets.strip
+    print "#{player}'s Marking Token: "
+    token = gets.strip
+    Player.new(name,token)
+  end
 end
 
 ### Troubleshooting and testing below
 
 ### Test start_game
-test_game = start_game
+test_game = Session.new()
 p test_game
+puts test_game.player1.name
+puts test_game.player1.token
+
 
 ### Test fill_board
 game = TicTacToe.new("a","b")
-board = game.fill_board([7,8,9,4,5,6,1,2,3])
-puts board
+board = game.print_board([7,8,9,4,5,6,1,2,3])
+
 #=>
 =begin
  7 | 8 | 9 
