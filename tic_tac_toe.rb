a5ef7dbdddb37488f70bcb404b9a9b10195b5fbb
Bob <bobdt84@gmail.com>
2022-04-23 22:28:30 -0500

    Create TicTacToe and Player classes

---
+++ tic_tac_toe.rb
@@ -0,0 +1,14 @@
+class TicTacToe
+   def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
+
+class Player
+  def initialize(name,toekn)
+    @name = name
+    @token = token
+  end
+end
+
