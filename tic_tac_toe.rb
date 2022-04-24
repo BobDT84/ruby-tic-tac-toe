ce7c8aa63dc3962cec064a6195c6e8dc399e4207
Bob <bobdt84@gmail.com>
2022-04-24 11:38:03 -0500

    Can now prompt the player for their square and update the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,7 +14,7 @@ class TicTacToe
       for i in 1...sub_array.length
         board.push "| #{sub_array[i]} "
       end
-      board.push "\n\t---" + "|---"*(width-1) + "\n" unless index == array.length-1
+      board.push "\n\t---" + "|---"*(width-1) + "\n" unless index == width-1
     end
     puts board.join()
   end
@@ -27,13 +27,15 @@ class Player
     @token = token
     @record = { won: 0, lost: 0, draw: 0}
   end
-
 end
 
 class Session < TicTacToe
   def initialize(board_width=3,board_height=3)
-    super register_player("Player1"), register_player("Player2")
+    #super register_player("Player1"), register_player("Player2")
+    #Can't figure out how to enter get prompts in debug console
+    super Player.new("jack","x"), Player.new("jill","o")
     @board = new_board(board_width,board_height)
+    player_turn(self.player1)
   end 
 
   private
@@ -48,11 +50,29 @@ class Session < TicTacToe
   def new_board(width,height)
     board = []
     for row in 1..height
-      board.push Array.new(width) {|index| (width*row)-index}.reverse
+      board.push Array.new(width) {|index| ((width*row)-index).to_s}.reverse
       # row 1 index 0 width 3 => (3*1)-0 => 3 
       # row 1 => [3,2,1].reverse => [1,2,3]
     end
     board.reverse
+  end
+
+  def player_turn(player)
+    self.print_board()
+    print "#{player.name} select a square: "
+    #square = gets.strip
+    square = "7"
+    puts "Jack selected: "
+    p square
+    self.update_board(square,player.token)
+    self.print_board()
+  end
+
+  def update_board(square,token)
+    for row in @board
+      index = row.index(square)
+      row[index] = token if index
+    end
   end
 
   public
